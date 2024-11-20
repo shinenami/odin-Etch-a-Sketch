bc7171345bd7fd19b3226e7b5006380fe6eb0af4
shinenami <102074489+shinenami@users.noreply.github.com>
2024-11-20 04:39:17 +0300

    add test grid

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 const board = document.getElementById("board")
+
+let rows = 32;
+let columns = 32;
+
+function updateSquareWidth (columns) {
+    document.documentElement.style.setProperty('--flex-item-width', `${100/columns}%`)
+}
+
+function createSquares(rows, columns = rows) {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            const square = document.createElement("div")
+            square.classList.add("board-square")
+            square.classList.add(`row:${r+1}`)
+            square.classList.add(`column:${c+1}`)
+            board.append(square)
+        }
+    }
+}
+
+updateSquareWidth(columns)
+createSquares(rows)
