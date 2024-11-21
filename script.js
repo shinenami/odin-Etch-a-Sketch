fe98c7335a423f2977861cdc2adbeaf8e55b15fc
shinenami <102074489+shinenami@users.noreply.github.com>
2024-11-21 09:05:32 +0300

    improve readability (hopefully(who knows))

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
+let rows = 8;
+let columns = 8;
+let isMouseDown = false;
+let isRGB = false;
+
 const board = document.getElementById("board")
 const body = document.querySelector("body")
 const inputSize = document.createElement("div")
 const tooltip = document.createElement("div")
-tooltip.id = "sliderTooltip"
-inputSize.id = "inputSize"
 const inputSizeField = inputSize.appendChild(document.createElement("input"))
+const inputSizeButton = inputSize.appendChild(document.createElement("button"))
+const rgbButton = document.createElement("input")
+const rgbLabel = document.createElement("label")
+
+rgbLabel.htmlFor = rgbButton
+rgbLabel.textContent = "rgb"
+rgbButton.prepend(rgbLabel)
 Object.assign(inputSizeField, {
     type : "range",
     // placeholder : "Input size",
     min : 1,
     max : 128,
-    value : 6,
+    value : 8,
     // title : `Grid Size`,
     oninput : function () {
         updateSliderTooltip(this)
     }
 })
-const inputSizeButton = inputSize.appendChild(document.createElement("button"))
+Object.assign(rgbButton, {
+    type: "checkbox",
+    textContent: "rgb"
+
+})
+tooltip.id = "sliderTooltip"
+inputSize.id = "inputSize"
 inputSizeButton.textContent = "Set"
 inputSize.appendChild(tooltip)
+// inputSize.appendChild(rgbButton)
 body.insertBefore(inputSize, board);
 // body.insertBefore(tooltip, board)
 
-
-let rows = 8;
-let columns = 8;
 
 function updateSliderTooltip(slider) {
     const sliderRect = slider.getBoundingClientRect()
     const tooltipPosition = ((slider.value - slider.min) / (slider.max - slider.min)) * sliderRect.width
 
     tooltip.textContent = slider.value
-
+    
     tooltip.style.left = `${tooltipPosition + sliderRect.left - sliderRect.width * 0}px`
-    tooltip.style.top = `${sliderRect.top + 36}px`
-    // tooltip.style.visibility = `visible`;
+    tooltip.style.top = `${sliderRect.top - 24}px`
 }
-
-inputSizeField.addEventListener('blur', () => { tooltip.style.visibility = 'hidden';})
-inputSizeField.addEventListener('mouseleave', () => { tooltip.style.visibility = 'hidden'; });
-inputSizeField.addEventListener('mouseenter', () => { updateSliderTooltip(inputSizeField); tooltip.style.visibility = 'visible'; });
 
 function updateSquaresWidth(columns) {
     document.documentElement.style.setProperty('--flex-item-width', `${100/columns}%`)
 }
-
 
 async function createSquares(rows, columns = rows) {
     const fragment = document.createDocumentFragment()
@@ -83,7 +91,34 @@ function updateGrid(rows) {
     createSquares(rows)
 }
 
-let isMouseDown = false;
+function colorize(e) {
+    if (e.target.id === "board") return;
+
+    let opacity = e.target.style.opacity;
+    const isColored = e.target.classList.contains("colored");
+
+    if (isColored && opacity == 1) return;
+    
+    if (isMouseDown) {
+        switch (isRGB) {
+            case true:
+                rgb = randomRGB();
+                e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+                break;
+            default:
+                e.target.style.backgroundColor = `red`
+        }
+        
+        e.target.classList.add("colored")
+
+        opacity = parseFloat(opacity) + 0.1;
+        e.target.style.opacity = opacity;        
+    }
+}
+
+inputSizeField.addEventListener('blur', () => { tooltip.style.visibility = 'hidden';})
+inputSizeField.addEventListener('mouseleave', () => { tooltip.style.visibility = 'hidden'; });
+inputSizeField.addEventListener('mouseenter', () => { updateSliderTooltip(inputSizeField); tooltip.style.visibility = 'visible'; });
 
 document.addEventListener("mousedown", () => {
     isMouseDown = true;
@@ -93,30 +128,10 @@ document.addEventListener("mouseup", () => {
     isMouseDown = false;
 })
 
-let isRGB = false;
-
-
-function colorize(e) {
-    if (e.target.id === "board") return;
-    let opacity = e.target.style.opacity;
-    const isColored = e.target.classList.contains("colored");
-    if (isColored && opacity === 1) return;
-
-    if (isMouseDown) {
-        switch (isRGB) {
-            case true:
-                rgb = randomRGB();
-                e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-            default:
-                e.target.style.backgroundColor = `red`
-        }
-        
-        e.target.classList.add("colored")
-        opacity = parseFloat(opacity) + 0.1;
-        e.target.style.opacity = opacity;        
-
-    }
-}
+rgbButton.addEventListener("change", () => {
+    isRGB = rgbButton.checked;
+    console.log(`isRGB: ${isRGB}`)
+})
 
 board.addEventListener("mouseover", colorize)
 board.addEventListener("mousedown", (e) => {
